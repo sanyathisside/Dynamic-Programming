02905f911ee9cb3f335de001087c65eb28ecd1b5
Sanya Kumari <55986045+sanya2508@users.noreply.github.com>
2020-01-21 00:58:13 +0530

    Create 10-k ordered lcs.cpp

---
+++ 10-k ordered lcs.cpp
@@ -0,0 +1,50 @@
+ #include <bits/stdc++.h>
+    #include<stdio.h>
+    using namespace std;
+    #define F(i,a,b) for(ll i = a; i <= b; i++)
+    #define ll long long
+    #define INF 1000000009
+    #define mod 1000000007
+     
+     
+    ll n,m,a[2005],b[2005];
+    ll dp[2005][2005][8];
+     
+     
+    ll f(ll i,ll j,ll k)
+    {
+    	if(i >= n || j >= m)
+    		return 0;
+    	if(dp[i][j][k] != -1)
+    		return dp[i][j][k];
+    	ll ret = 0;
+    	if(a[i] == b[j])
+    		ret = 1 + f(i+1,j+1,k);
+    	else 
+    	{
+    		if(k > 0)
+    			ret = 1 + f(i+1,j+1,k-1);
+    		ret = max(ret,f(i+1,j,k));
+    		ret = max(ret,f(i,j+1,k));
+    	}
+    	return dp[i][j][k] = ret;
+    }
+    int main() 
+    {
+    	
+    	memset(dp,-1,sizeof(dp));
+    	ll k;
+    	cin>>n>>m>>k;
+    	
+    	F(i,0,n-1)
+    		cin>>a[i];
+    	
+    	F(i,0,m-1)
+    		cin>>b[i];
+    	
+    	ll ans = f(0,0,k);
+    	cout<<ans<<endl;
+    	
+    	
+    	return 0;
+    }
