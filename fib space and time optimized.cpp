1ca61c5a82dcb63ac6dc2c2841fbc5bfeda35074
Sanya Kumari <1701sanya@gmail.com>
2020-05-11 12:19:43 +0530

    Create fib space and time optimized.cpp

---
+++ fib space and time optimized.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int fib(int n){
+    if(n==0 or n==1){
+        return n;
+    }
+    int a=0;
+    int b=1;
+    int c;
+    for(int i=2; i<=n; i++){
+    c=a+b;
+    a=b;
+    b=c;
+    }
+    return c;
+}
+int main(){
+	int n;
+	cin>>n;
+	cout<<fib(n)<<endl;
+}
